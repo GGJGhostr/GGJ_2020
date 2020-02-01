7b6996697d00d532e1a766fd17c520524b32589a
Louis <me@louisschmidt.com>
2020-02-01 19:53:10 +0900

    Add bullet

---
+++ Assets/Scripts/Core/Character/CharacterShooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterShooting : MonoBehaviour
+{
+    [SerializeField] Transform m_bulletSpawnerTransform = null;
+    [SerializeField] GameObject m_bulletPrefab = null;
+
+    private void Awake()
+    {
+        
+    }
+
+    public void ShootBullet(Vector2 dir)
+    {
+        if (dir == Vector2.zero)
+            dir = new Vector2(transform.right.x, 0f);
+
+        float angle = Vector2.Angle(new Vector2(transform.right.x, 0f), dir);
+        Quaternion quat = Quaternion.AngleAxis(angle, Vector3.forward);
+        GameObject clone = Instantiate(m_bulletPrefab, m_bulletSpawnerTransform.transform.position, quat);
+        clone.GetComponent<Bullet>().Fire(dir);
+        //bullet.Fire(dir);
+    }
+}
