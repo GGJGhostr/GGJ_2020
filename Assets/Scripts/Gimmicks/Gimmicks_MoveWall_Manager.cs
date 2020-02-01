c43a6d558c5729835a3d08d7e751cbe495900a9e
m011709001 <m011709001@edu.teu.ac.jp>
2020-02-02 02:50:40 +0900

    player move wall create

---
+++ Assets/Scripts/Gimmicks/Gimmicks_MoveWall_Manager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gimmicks_MoveWall_Manager : MonoBehaviour
+{
+    private bool playerTouch = false;
+    public bool PlayerTouch
+    {
+        get { return playerTouch; }
+        set { playerTouch = value; }
+    }
+    private bool moveWall = true;
+    public bool MoveWall
+    {
+        get { return moveWall; }
+        set { moveWall = value; }
+    }
+}
