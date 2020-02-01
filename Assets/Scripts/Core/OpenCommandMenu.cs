a1776ed6a606d6046aa464a5c125240394d512b2
Scarhi <e.saoud.gd@gmail.com>
2020-02-01 23:01:15 +0900

    textbox opens and is in focus by pressing enter, can be used to input commands

---
+++ Assets/Scripts/Core/OpenCommandMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class OpenCommandMenu : MonoBehaviour
+{
+
+    public GameObject cmdZone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForInputs();
+    }
+
+    void CheckForInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            cmdZone.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(cmdZone);
+        }
+
+    }
+
+    public void EndEdit()
+    {
+        cmdZone.GetComponent<InputField>().text = string.Empty;
+    }
+}
