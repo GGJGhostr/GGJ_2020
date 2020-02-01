11bfcd64752f5a17ab7dbf8bf30cdd638c8fc01e
m011709001 <m011709001@edu.teu.ac.jp>
2020-02-02 06:35:18 +0900

    調整

--- Assets/Sounds/Sound_BGM.cs
+++ Assets/Sounds/Sound_BGM.cs
@@ -5,33 +5,32 @@ using UnityEngine;
 public class Sound_BGM : MonoBehaviour
 {
     
-    [SerializeField] private float transitionTime =1.0f;
+    [SerializeField] private float delayTime = 1.0f;
     [SerializeField] private float startVolume = 0.0f;
-    [SerializeField] private float maxVolume = 1.0f;
+    [SerializeField] [Range(0,1)]private float endVolume = 1.0f;
     private AudioSource audioSource;
     void Start()
     {
        audioSource= this.gameObject.GetComponent<AudioSource>();
         audioSource.volume = 0;
-        StartCoroutine(ChangeSoundVolume(startVolume,maxVolume));
+        StartCoroutine(ChangeSoundVolume(startVolume,endVolume, delayTime));
     }
 
-    IEnumerator ChangeSoundVolume(float startVolume,float maxVolume)
+    IEnumerator ChangeSoundVolume(float startVolume,float endVolume,float delayTime)
     {
-        var time = startVolume * transitionTime ;
-        while (transitionTime*maxVolume > time)
+        var time = delayTime*audioSource.volume;
+        while (delayTime > time)
         {
-            audioSource.volume = time / transitionTime;
+            audioSource.volume = Mathf.Lerp(startVolume, endVolume, time/delayTime);
             time += Time.deltaTime;
             yield return null;
         }
-        audioSource.volume = maxVolume;
+        audioSource.volume = endVolume;
     }
 
     public void ChangeSound(float maxSound)
     {
         if (maxSound > 1.0f) maxSound = 1.0f;
-        StartCoroutine(ChangeSoundVolume(maxVolume, maxSound));
-        maxVolume = maxSound;
+        StartCoroutine(ChangeSoundVolume(audioSource.volume, maxSound,delayTime));
     }
 }
