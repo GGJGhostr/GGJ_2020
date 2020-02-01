856f866e2d9ee828add1c7f8d7a76222d495c8f9
m011709001 <m011709001@edu.teu.ac.jp>
2020-02-02 08:37:36 +0900

    最低限の動きができる

--- Assets/Scripts/SceneHandlers/SceneScoringHandler.cs
+++ Assets/Scripts/SceneHandlers/SceneScoringHandler.cs
@@ -7,6 +7,7 @@ public class SceneScoringHandler : MonoBehaviour
 
     public CharacterScoring P1_CharacterScoring = null;
     public CharacterScoring P2_CharacterScoring = null;
+    [SerializeField] private Loadings loadings = null;
 
     public int P1_Score { get; private set; }
     public int P2_Score { get; private set; }
@@ -30,6 +31,7 @@ public class SceneScoringHandler : MonoBehaviour
              * ここで次のシーンをロードします。
              * Koko de tsugi no shīn o rōdo shimasu.
              */
+            loadings.LoadingScene("Result");
             Debug.Log("We have a Winner");
         }
 
