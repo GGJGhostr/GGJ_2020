05a73fc3cb64bae9b2a7537d9aeb39aec9f9dd41
オウ　キョク <m02171017e@edu.teu.ac.jp>
2020-02-01 15:10:05 +0900

    add dynamic cloud demo

---
+++ Assets/Scripts/Core/Background/BgController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class BgController : MonoBehaviour
+{
+    public GameObject[] cloud_prefab;
+    public int cloud_num_scale = 2;
+    private GameObject[] cloud_obj;
+    public Vector2 cloud_random_range_Y;
+    public float cloud_start_X;
+    public float cloud_interval_X;
+    public Vector2 cloud_random_range_speed;
+
+    private Vector3[] startPos;
+    public float speed = -10.0f;
+    public float cycle_length = 30.0f;
+    public float smoothTime = 0.3F;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void RandCloudPos(int idx)
+    {
+        float random_seed = Random.Range(cloud_random_range_speed.x, cloud_random_range_speed.y);
+        float cloud_x = cloud_start_X + cloud_interval_X * random_seed * idx;
+        float cloud_y = Random.Range(cloud_random_range_Y.x, cloud_random_range_Y.y);
+        startPos[idx] = new Vector3(cloud_x, cloud_y, 0.0f);
+        cloud_obj[idx].transform.position = startPos[idx];
+    }
+    void Awake()
+    {
+        cloud_obj = new GameObject[cloud_prefab.Length * cloud_num_scale];
+        startPos = new Vector3[cloud_prefab.Length * cloud_num_scale];
+        for (int i = 0; i < cloud_prefab.Length * cloud_num_scale; i++)
+        {
+            cloud_obj[i] = GameObject.Instantiate(cloud_prefab[i % cloud_prefab.Length], Vector3.zero, Quaternion.identity);
+            RandCloudPos(i);
+        }
+    }
+
+    private void UpdateCloudPos()
+    {
+        for (int i = 0; i < cloud_obj.Length; i++)
+        {
+            float random_seed = Random.Range(cloud_random_range_speed.x, cloud_random_range_speed.y);
+            Vector3 target = new Vector3(cloud_obj[i].transform.position.x + speed * random_seed, cloud_obj[i].transform.position.y, 0.0f);
+            cloud_obj[i].transform.position = Vector3.SmoothDamp(cloud_obj[i].transform.position, target, ref velocity, smoothTime);
+            float move_length = Mathf.Abs(cloud_obj[i].transform.position.x - startPos[i].x);
+            if (move_length > cycle_length)
+            {
+                RandCloudPos(i);
+            }
+        }
+    }
+
+    void Update()
+    {
+        UpdateCloudPos();
+    }
+}
