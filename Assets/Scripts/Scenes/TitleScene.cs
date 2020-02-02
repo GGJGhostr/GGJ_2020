02687fdeb9f8e1f29abe67710e49eb6d4c4f3143
オウ　キョク <m02171017e@edu.teu.ac.jp>
2020-02-02 11:25:20 +0900

    update title scene

--- Assets/Scripts/Scenes/TitleScene.cs
+++ Assets/Scripts/Scenes/TitleScene.cs
@@ -9,19 +9,20 @@ public class TitleScene : MonoBehaviour
     public GamePad.PlayerIndex player_idx = GamePad.PlayerIndex.One;
     public GamePad.PlayerIndex player_idxtwo = GamePad.PlayerIndex.Two;
     [SerializeField] private Loadings loadings;
-    private bool loadingStart=false;
-    
+    private bool loadingStart = false;
+
+    public string scene_name = "MainGame";
+
     void Update()
     {
         GamepadState player_state = GamePad.GetState(player_idx);
         GamepadState player_statetwo = GamePad.GetState(player_idxtwo);
         if (player_state.A || player_state.B || player_state.X || player_state.Y ||
-            player_statetwo.A || player_statetwo.B || player_statetwo.X || player_statetwo.Y)
+            player_statetwo.A || player_statetwo.B || player_statetwo.X || player_statetwo.Y || Input.GetKeyDown("space"))
         {
-            //SceneManager.LoadScene("CharacterSelect");
             if (!loadingStart)
             {
-                loadings.LoadingScene("CharacterSelect");
+                loadings.LoadingScene(scene_name);
                 loadingStart = true;
             }
         }
