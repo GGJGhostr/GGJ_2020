99507f10b22a134999314afb622155b53e34cc75
m011709001 <m011709001@edu.teu.ac.jp>
2020-02-02 09:14:47 +0900

    sound_effect　create

---
+++ Assets/Sounds/AudioClipScriptable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "UI/AudioClip", fileName = "AudioClip")]
+
+public class AudioClipScriptable : ScriptableObject
+{
+    [SerializeField] private AudioClip[] _audioClip = null;
+
+    public AudioClip[] audioClip => _audioClip;
+}
---
+++ Assets/Sounds/Sound_Effect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GamepadInput;
+
+public class Sound_Effect : MonoBehaviour
+{
+    [SerializeField] [Range(0, 1)] private float volume = 1.0f;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClipScriptable clips;
+    private GamePad.PlayerIndex player_idx = GamePad.PlayerIndex.Any;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+        audioSource.volume = volume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //debug
+        GamepadState player_state = GamePad.GetState(player_idx);
+        if (player_state.X)
+        {
+            PlayEffectSound(0);
+        }
+    }
+
+    public void ChangeVolume(float volumeSize)
+    {
+        volume = volumeSize;
+        audioSource.volume = volume;
+    }
+
+    public void PlayEffectSound(int num)
+    {
+        audioSource.PlayOneShot(clips.audioClip[num]);
+    }
+}
